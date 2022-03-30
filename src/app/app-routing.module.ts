dbfcf976b0068156087719d8717a51eccb73164c
Kyle R. Miller <krm8mc@mst.edu>
2022-03-29 19:56:04 -0500

    Adding google login and creating basic login guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+import {PlaceSwipperComponent} from "./place-swipper/place-swipper.component";
+import {AuthGuardService} from "./auth-guard.service";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'mainpage', component: PlaceSwipperComponent, canActivate: [AuthGuardService]},
+  {path: '**', component: LoginComponent}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaceSwipperComponent } from './place-swipper/place-swipper.component';
 import { PlaceElementComponent } from './place-swipper/place-element/place-element.component';
 import { HeaderComponent } from './header/header.component';
+import {GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from "angularx-social-login";
+import { LoginComponent } from './login/login.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     PlaceSwipperComponent,
     PlaceElementComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('232182691200-f8vubd7t0nvae2hdhluqjkkr1qnq86dr.apps.googleusercontent.com'),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
